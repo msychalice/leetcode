77c4daf0fa47639afe9de0fb99b467e3704e040b
Siyang <siyang.msy@gmail.com>
2020-11-13 09:15:06 -0500

    58

---
+++ 58LengthOfLastWord.cpp
@@ -0,0 +1,147 @@
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+template<class Container>
+void printContainer(const Container& container) {
+//    return;
+
+    if (container.size() == 0) {
+        cout << "[]" << endl;
+        return;
+    }
+
+    for (auto it = container.begin(); it != container.end(); it++) {
+        if (it == container.begin()) {
+            cout << "[";
+        }
+        cout << *it;
+        if (it == prev(container.end())) {
+            cout << "]" << endl;
+        } else {
+            cout << ", ";
+        }
+    }
+}
+
+template<class T>
+void print2DimVec(const vector<vector<T>>& container) {
+//    return;
+
+    if (container.size() == 0) {
+        cout << "[]" << endl;
+        return;
+    }
+
+    for (auto it = container.begin(); it != container.end(); it++) {
+        if (it == container.begin()) {
+            cout << "[" << endl;
+        }
+        printContainer(*it);
+        if (it == prev(container.end())) {
+            cout << "]" << endl;
+        }
+    }
+}
+
+template<class T>
+void printMap(const T& container) {
+//    return;
+
+    cout << "size " << container.size() << endl;
+    if (container.size() == 0) {
+        cout << "[]" << endl;
+        return;
+    }
+    for (auto it = container.begin(); it != container.end(); it++) {
+        if (it == container.begin()) {
+            cout << "[ ";
+        }
+
+        cout << "[" << it->first << ", " << it->second << "]";
+
+        if (next(it) == container.end()) {
+            cout << " ]" << endl;
+        } else {
+            cout << ", ";
+        }
+    }
+}
+
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void printList(const ListNode* node) {
+    while (node != nullptr) {
+        cout << node->val;
+
+        node = node->next;
+        
+        if (node != nullptr) {
+            cout << "->";
+        } else {
+            cout << endl;
+        }
+    }
+}
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        if (s.size() == 0) {
+            return 0;
+        }
+
+        int cur = s.find_last_not_of(' ');
+        if (cur == string::npos) {
+            return 0;
+        }
+
+        int lenLastWord = 0;
+        for (; cur >= 0; cur--) {
+            if (s[cur] != ' ') {
+                lenLastWord++;
+            } else {
+                break;
+            }
+        }
+        return lenLastWord;
+    }
+};
+
+int main() {
+    Solution s;
+
+    auto output = s.lengthOfLastWord("Hello World");
+    auto output1 = s.lengthOfLastWord("a");
+
+    cout << "*************output*************" << endl;
+    cout << output << endl;
+    cout << output1 << endl;
+
+    return 0;
+}
