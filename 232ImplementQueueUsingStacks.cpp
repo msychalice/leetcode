a69c6f145f9a02dc23f96e8f1613425eff01a1a2
Siyang <siyang.msy@gmail.com>
2020-12-10 11:52:58 -0500

    232 cpp

---
+++ 232ImplementQueueUsingStacks.cpp
@@ -0,0 +1,204 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+template<class Container>
+void printContainer(const Container& container) {
+//    return;
+
+    if (container.size() == 0) {
+        cout << "[]" << endl;
+        return;
+    }
+
+    for (auto it = container.begin(); it != container.end(); it++) {
+        if (it == container.begin()) {
+            cout << "[";
+        }
+        cout << *it;
+        if (it == prev(container.end())) {
+            cout << "]" << endl;
+        } else {
+            cout << ", ";
+        }
+    }
+}
+
+template<class T>
+void print2DimVec(const vector<vector<T>>& container) {
+//    return;
+
+    if (container.size() == 0) {
+        cout << "[]" << endl;
+        return;
+    }
+
+    for (auto it = container.begin(); it != container.end(); it++) {
+        if (it == container.begin()) {
+            cout << "[" << endl;
+        }
+        printContainer(*it);
+        if (it == prev(container.end())) {
+            cout << "]" << endl;
+        }
+    }
+}
+
+template<class T>
+void printMap(const T& container) {
+//    return;
+
+    cout << "size " << container.size() << endl;
+    if (container.size() == 0) {
+        cout << "[]" << endl;
+        return;
+    }
+    for (auto it = container.begin(); it != container.end(); it++) {
+        if (it == container.begin()) {
+            cout << "[ ";
+        }
+
+        cout << "[" << it->first << ", " << it->second << "]";
+
+        if (next(it) == container.end()) {
+            cout << " ]" << endl;
+        } else {
+            cout << ", ";
+        }
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+void printList(const ListNode* node) {
+    while (node != nullptr) {
+        cout << node->val;
+
+        node = node->next;
+        
+        if (node != nullptr) {
+            cout << "->";
+        } else {
+            cout << endl;
+        }
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+void printTree(TreeNode* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    cout << root->val << endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+    Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
+    Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {}
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        m_stkPush.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        movePushStkToPopStk();
+
+        int ret = m_stkPop.top();
+        m_stkPop.pop();
+        return ret;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        movePushStkToPopStk();
+        return m_stkPop.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return m_stkPush.empty() && m_stkPop.empty();
+    }
+
+private:
+    void movePushStkToPopStk() {
+        if (m_stkPop.empty()) {
+            while (!m_stkPush.empty()) {
+                m_stkPop.push(m_stkPush.top());
+                m_stkPush.pop();
+            }
+        }
+    }
+
+    stack<int> m_stkPush;
+    stack<int> m_stkPop;
+};
+
+int main() {
+    MyQueue* obj = new MyQueue();
+    obj->push(1);
+    cout << obj->pop() << endl;
+    cout << obj->peek() << endl;
+    cout << obj->empty() << endl;
+
+    cout << "*************output*************" << endl;
+
+    return 0;
+}
